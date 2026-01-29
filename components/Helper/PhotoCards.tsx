7cea0a6a8dd3b4dcd6a9d341f7ac0ef7c67feefe
really-riel <110791013+really-riel@users.noreply.github.com>
2026-01-29 02:47:47 +0100

    Update PhotoCards.tsx

--- components/Helper/PhotoCards.tsx
+++ components/Helper/PhotoCards.tsx
@@ -11,11 +11,11 @@ type Props = {
 
 const PhotoCards = ({ ImageData }: Props) => {
   return (
-    <div className="rounded-t-2xl  shadow bg-blue-200-50 overflow-hidden w-full  border-b-2">
+    <div className="rounded-t-2xl  shadow bg-blue-200-50 overflow-hidden w-[360px] mx-auto border-b-2">
       <img
         alt="Image"
         src={ImageData.image}
-        className=" object-cover w-full h-[530px]"
+        className=" object-cover w-full h-[500px]"
       />
       <h3 className="p-2">{ImageData.caption}</h3>
     </div>
